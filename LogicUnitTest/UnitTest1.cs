40301d519487526390575faa719387866fde159d
Miłosz Kowalczyk <247705@edu.p.lodz.pl>
2024-04-14 15:10:54 +0200

    Sth wrong with test config

---
+++ LogicUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Logic;
+namespace LogicUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
