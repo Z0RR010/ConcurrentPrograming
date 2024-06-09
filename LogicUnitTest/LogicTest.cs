5a1699988e1290e621c8a1d34580210808e6481c
Bartłomiej Wieloch <247820@edu.p.lodz.pl>
2024-06-09 21:16:03 +0200

    Fixed tests

--- LogicUnitTest/LogicTest.cs
+++ LogicUnitTest/LogicTest.cs
@@ -25,7 +25,7 @@ namespace LogicUnitTest
                 public override IBallType GetBall(Vector2 position, Vector2 speed, Table table)
                 {
                     var ball = new TestBall(position, speed, table);
-                    TestBalls.Add(ball);
+                    //TestBalls.Add(ball);
                     return ball;
                 }
 
@@ -107,10 +107,12 @@ namespace LogicUnitTest
                 public void LogicApi_CreatesBalls()
                 {
                     var logicApi = LogicAbstractApi.CreateApi(dataApi);
-                    int ballCount = 5;
+                    int ballCount = 3;
                     logicApi.GenerateHandler(ballCount);
 
-                    var (positions, radii) = logicApi.GetBallInfo();
+                    var ret = logicApi.GetBallInfo();
+                    var positions = ret.Item1;
+                    var radii = ret.Item2;
                     Assert.AreEqual(ballCount, positions.Count);
                     Assert.AreEqual(ballCount, radii.Count);
                 }
@@ -121,10 +123,22 @@ namespace LogicUnitTest
                     var logicApi = LogicAbstractApi.CreateApi(dataApi);
                     int ballCount = 1;
                     logicApi.GenerateHandler(ballCount);
+                    var test = new List<TestBallPositionUpdateHandler>();
+                    var handlers = new List<EventHandler<ReadOnlyCollection<float>>>();
+                    foreach (var i in Enumerable.Range(0, ballCount))
+                    {
+                        test.Add(new TestBallPositionUpdateHandler());
+                        handlers.Add(test.ElementAt(i).HandlePositionUpdate);
+                    }
+                    logicApi.ConnectBalls(handlers);
+                    logicApi.Stop();
                     Thread.Sleep(100);
                     logicApi.Stop();
-
-                    Assert.IsTrue(positionUpdateHandler.EventTriggered);
+                    foreach (var t in test)
+                    {
+                        Assert.IsTrue(t.EventTriggered);
+                    }
+                    
                 }
 
                 [Test]
@@ -133,7 +147,7 @@ namespace LogicUnitTest
                     var logicApi = LogicAbstractApi.CreateApi(dataApi);
                     int ballCount = 3;
                     logicApi.GenerateHandler(ballCount);
-                    logicApi.Stop();
+
 
                     var (positions, radii) = logicApi.GetBallInfo();
                     Assert.AreEqual(0, positions.Count);
